95fa1ef7c0344eb5cff678de610d6198bd526ff0
Lomoee <favoursunday600@gmail.com>
2024-03-04 21:35:53 +0100

    login-page done

--- src/Auth/login.js
+++ src/Auth/login.js
@@ -21,25 +21,35 @@ function Login () {
                 </div>
             </div>
 
-            <div className="flex justify-center mt-16">
+            <div className="flex justify-center mt-16 mb-10">
 
-                <div className="bg-white container mx-80 px-3 rounded-xl">
-                    <h1 className="mt-5 text-red-600 text-4xl font-semibold ">Sign In</h1>
+                <div className="bg-white container mx-80 max-sm:mx-2 px-3 rounded-xl">
+                    <h1 className="mt-5 text-red-600 text-4xl font-semibold ">Log In</h1>
 
                     <div className="group mt-5 flex justify-center ">
-                        <svg class="icon" xmlns="http://www.w3.org/2000/svg" width="30" height="32" viewBox="0 0 24 24" fill="#0000"><path d="M12 12a5 5 0 1 0 0-10 5 5 0 0 0 0 10ZM20.59 22c0-3.87-3.85-7-8.59-7s-8.59 3.13-8.59 7" stroke="red" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path></svg>
-                        <input className="input w-4/5" type="text" placeholder="Username" />
+                        <svg class="icon left-20 max-sm:left-5" xmlns="http://www.w3.org/2000/svg" width="30" height="32" viewBox="0 0 24 24" fill="#0000"><path d="M12 12a5 5 0 1 0 0-10 5 5 0 0 0 0 10ZM20.59 22c0-3.87-3.85-7-8.59-7s-8.59 3.13-8.59 7" stroke="red" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path></svg>
+                        <input className="input w-4/5 max-sm:w-full" type="text" placeholder="Username" />
                     </div>
 
                     <div className="group mt-5 flex justify-center">
-                    <svg class="icon" xmlns="http://www.w3.org/2000/svg" width="30" height="32" viewBox="0 0 24 24" fill="none"><path d="M10.49 2.23 5.5 4.11c-1.15.43-2.09 1.79-2.09 3.01v7.43c0 1.18.78 2.73 1.73 3.44l4.3 3.21c1.41 1.06 3.73 1.06 5.14 0l4.3-3.21c.95-.71 1.73-2.26 1.73-3.44V7.12c0-1.23-.94-2.59-2.09-3.02l-4.99-1.87c-.85-.31-2.21-.31-3.04 0Z" stroke="#FF8A65"stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path><path d="M12 12.5a2 2 0 1 0 0-4 2 2 0 0 0 0 4ZM12 12.5v3" stroke="red" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"></path></svg>
-                        <input className="input w-4/5" type="password" placeholder="password" />
+                    <svg class="icon left-20 max-sm:left-5" xmlns="http://www.w3.org/2000/svg" width="30" height="32" viewBox="0 0 24 24" fill="none"><path d="M10.49 2.23 5.5 4.11c-1.15.43-2.09 1.79-2.09 3.01v7.43c0 1.18.78 2.73 1.73 3.44l4.3 3.21c1.41 1.06 3.73 1.06 5.14 0l4.3-3.21c.95-.71 1.73-2.26 1.73-3.44V7.12c0-1.23-.94-2.59-2.09-3.02l-4.99-1.87c-.85-.31-2.21-.31-3.04 0Z" stroke="#FF8A65"stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path><path d="M12 12.5a2 2 0 1 0 0-4 2 2 0 0 0 0 4ZM12 12.5v3" stroke="red" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"></path></svg>
+                        <input className="input w-4/5 max-sm:w-full" type="password" placeholder="password" />
                     </div>
 
 
-                    <div class="cntrrr">
-                        <input checked="" type="checkbox" id="cbx" class="hidden-xs-uppp" />
-                        <label for="cbx" class="cbx"></label>
+                    <div class="mt-5 float-right mr-14 max-sm:mr-2">
+                        <input type="checkbox" name="rememberme"/>
+                        <label for="rememberme" class="text-red-500 font-semibold">Rremember Me</label>
+                    </div>
+
+                    <div className="mt-20">
+                        <button className="bg-red-400 text-white w-72 h-10 rounded text-lg" >Log In</button>
+                    </div>
+
+                    <div className="mt-10 mb-5">
+                        <Link className="ml-10 underline max-sm:ml-0">Register</Link>
+
+                        <Link className="float-right mr-40 underline max-sm:mr-10">forgot password?</Link>
                     </div>
 
                 </div>
