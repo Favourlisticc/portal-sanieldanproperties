d21d3853bdec5cd5de44be1c12bf85974a054fba
Lomoee <favoursunday600@gmail.com>
2024-03-06 10:51:40 +0100

    login setup

--- src/Auth/login.js
+++ src/Auth/login.js
@@ -1,6 +1,105 @@
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
+import React, { useState } from 'react';
+
+
 
 function Login () {
+
+    const navigate = useNavigate();
+    const [usernameempty, setUsernameEmpty] = useState('');
+    const [usernamematch, setUsernameMatch] = useState('');
+    const [passwordempty, setPasswordEmpty] = useState('');
+    const [passwordmatch, setPasswordMatch] = useState('');
+    const [error, setError] = useState('');
+
+    const [formData, setFormData] = useState({
+
+        name: '',
+        password: '',
+
+      });
+
+      const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({ ...formData, [name]: value });
+        // Check if input is empty and set corresponding state
+
+    };
+
+      console.log(formData)
+
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await fetch(
+                // 'http://localhost:3001/auth/login',
+                'https://portal-sanieldanproperties-api.onrender.com/auth/login',
+                {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(formData),
+            });
+    
+            if (response.ok) {
+                const data = await response.json(); // Assuming response contains JSON data
+                console.log('Login successful:', data);
+                // Handle successful signup, e.g., redirect user to another page or show a success message
+                navigate("/dashboard");
+            } else {
+                const responseData = await response.json();
+                if (response.status === 400) {
+                    if (responseData.error === 'Please provide Username') {
+                        setUsernameEmpty(responseData.error);
+
+                        setTimeout(() => {
+                            setUsernameEmpty('');
+                        }, 5000);
+
+                    } else if (responseData.error === 'Username does not exists') {
+                        setUsernameMatch(responseData.error);
+
+                        setTimeout(() => {
+                            setUsernameMatch('');
+                        }, 5000);
+
+                    } else if (responseData.error === 'Please provide Password') {
+                        setPasswordEmpty(responseData.error);
+
+                        setTimeout(() => {
+                            setPasswordEmpty('');
+                        }, 5000);
+
+                    } else if (responseData.error === 'Password does not match') {
+                        setPasswordMatch(responseData.error);
+
+                        setTimeout(() => {
+                            setPasswordMatch('');
+                        }, 5000);
+
+                    }else {
+                        setError(responseData.error);
+
+                        setTimeout(() => {
+                            setError('');
+                        }, 5000);
+                    }
+                } else {
+                    setError('Failed to sign up');
+
+                    setTimeout(() => {
+                        setError('');
+                    }, 5000);
+                }
+            }
+        } catch (error) {
+            console.log('Error signing up:', error);
+            // Handle network errors or other exceptions
+        }
+    };
     return(
         <div className="">
             <div className="background-image text-white ml-auto mr-auto">
@@ -21,20 +120,28 @@ function Login () {
                 </div>
             </div>
 
+            <form onSubmit={handleSubmit}>
+
             <div className="flex justify-center mt-16 mb-10">
+
 
                 <div className="bg-white container mx-80 max-sm:mx-2 px-3 rounded-xl">
                     <h1 className="mt-5 text-red-600 text-4xl font-semibold ">Log In</h1>
 
                     <div className="group mt-5 flex justify-center ">
                         <svg class="icon left-20 max-sm:left-5" xmlns="http://www.w3.org/2000/svg" width="30" height="32" viewBox="0 0 24 24" fill="#0000"><path d="M12 12a5 5 0 1 0 0-10 5 5 0 0 0 0 10ZM20.59 22c0-3.87-3.85-7-8.59-7s-8.59 3.13-8.59 7" stroke="red" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path></svg>
-                        <input className="input w-4/5 max-sm:w-full" type="text" placeholder="Username" />
+                        <input className="input w-4/5 max-sm:w-full" type="text" placeholder="Username" name="username" onChange={handleChange} value={formData.username} />
                     </div>
+                    {usernameempty && <p className="text-red-500 mb-3 absolute" style={{zIndex: "999"}}>{usernameempty}</p>}
+                    {usernamematch && <p className="text-red-500 mb-3 absolute" style={{zIndex: "999"}}>{usernamematch}</p>}
+
 
                     <div className="group mt-5 flex justify-center">
                     <svg class="icon left-20 max-sm:left-5" xmlns="http://www.w3.org/2000/svg" width="30" height="32" viewBox="0 0 24 24" fill="none"><path d="M10.49 2.23 5.5 4.11c-1.15.43-2.09 1.79-2.09 3.01v7.43c0 1.18.78 2.73 1.73 3.44l4.3 3.21c1.41 1.06 3.73 1.06 5.14 0l4.3-3.21c.95-.71 1.73-2.26 1.73-3.44V7.12c0-1.23-.94-2.59-2.09-3.02l-4.99-1.87c-.85-.31-2.21-.31-3.04 0Z" stroke="#FF8A65"stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path><path d="M12 12.5a2 2 0 1 0 0-4 2 2 0 0 0 0 4ZM12 12.5v3" stroke="red" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"></path></svg>
-                        <input className="input w-4/5 max-sm:w-full" type="password" placeholder="password" />
+                        <input className="input w-4/5 max-sm:w-full" name="password" type="password" placeholder="password" onChange={handleChange} value={formData.password} />
                     </div>
+                    {passwordempty && <p className="text-red-500 mb-3 absolute" style={{zIndex: "999"}}>{passwordempty}</p>}
+                    {passwordmatch && <p className="text-red-500 mb-3 absolute" style={{zIndex: "999"}}>{passwordmatch}</p>}
 
 
                     <div class="mt-5 float-right mr-14 max-sm:mr-2">
@@ -42,19 +149,23 @@ function Login () {
                         <label for="rememberme" class="text-red-500 font-semibold">Rremember Me</label>
                     </div>
 
+                    {error && <p className="text-red-500 mb-3">{error}</p>}
+
                     <div className="mt-20">
-                        <button className="bg-red-400 text-white w-72 h-10 rounded text-lg" >Log In</button>
+                        <button type="submit" className="bg-red-400 text-white w-72 h-10 rounded text-lg" >Log In</button>
                     </div>
 
                     <div className="mt-10 mb-5">
                         <Link to="/signup"  className="ml-10 underline max-sm:ml-0">Register</Link>
 
-                        <Link className="float-right mr-40 underline max-sm:mr-10">forgot password?</Link>
+                        <Link to="/forgot-password" className="float-right mr-40 underline max-sm:mr-10">forgot password?</Link>
                     </div>
 
                 </div>
 
+
             </div>
+            </form>
         </div>
     )
 }
--- src/Auth/signup.js
+++ src/Auth/signup.js
@@ -2,9 +2,11 @@ import { Link, useNavigate } from "react-router-dom"
 import React, { useState } from 'react';
 import CountrySelect from "../countryselect";
 
-function Login () {
+function Signup () {
     const navigate = useNavigate();
     const [confirmPasswordError, setConfirmPasswordError] = useState('');
+    const [emailError, setEmailError] = useState('');
+    const [usernameError, setUsernameError] = useState('');
     const [error, setError] = useState('');
     const [formData, setFormData] = useState({
         username: '',
@@ -15,18 +17,49 @@ function Login () {
         confirmPassword: '',
         country: ''
       });
-    
-      const handleChange = (e) => {
-        setFormData({ ...formData, [e.target.name]: e.target.value });
-      };
+
+      const [emptyUsername, setEmptyUsername] = useState(false);
+    const [emptyEmail, setEmptyEmail] = useState(false);
+    const [emptyFirstName, setEmptyFirstName] = useState(false);
+    const [emptyLastName, setEmptyLastName] = useState(false);
+    const [emptyPassword, setEmptyPassword] = useState(false);
+    const [emptyConfirmPassword, setEmptyConfirmPassword] = useState(false);
+    const [emptyCountry, setEmptyCountry] = useState(false);
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({ ...formData, [name]: value });
+        // Check if input is empty and set corresponding state
+        switch (name) {
+            case 'username':
+                setEmptyUsername(value === '');
+                break;
+            case 'email':
+                setEmptyEmail(value === '');
+                break;
+            case 'firstName':
+                setEmptyFirstName(value === '');
+                break;
+            case 'lastName':
+                setEmptyLastName(value === '');
+                break;
+            case 'password':
+                setEmptyPassword(value === '');
+                break;
+            case 'confirmPassword':
+                setEmptyConfirmPassword(value === '');
+                break;
+            case 'country':
+                setEmptyCountry(value ==='');
+                break
+            default:
+                break;
+        }
+    };
 
       console.log(formData)
 
-    const [selectedCountry, setSelectedCountry] = useState(null);
 
-    const handleCountryChange = (selectedOption) => {
-        setFormData({ ...formData, country: selectedOption });
-    };
 
     // Function to validate the confirm password
    // Function to validate the confirm password
@@ -56,7 +89,10 @@ const validateConfirmPassword = () => {
         }
     
         try {
-            const response = await fetch('http://localhost:3001/auth/signup', {
+            const response = await fetch(
+                // 'http://localhost:3001/auth/signup',
+                'https://portal-sanieldanproperties-api.onrender.com/auth/signup',
+                {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
@@ -70,16 +106,37 @@ const validateConfirmPassword = () => {
                 // Handle successful signup, e.g., redirect user to another page or show a success message
                 navigate("/dashboard");
             } else {
-                 const responseData = await response.json();
-            if (response.status === 400) {
-                setTimeout(() => {
-                    setError(responseData.error);
-                }, 3000);
+                const responseData = await response.json();
+                if (response.status === 400) {
+                    if (responseData.error === 'Email is taken') {
+                        setEmailError(responseData.error);
 
-            } else {
-                setError('Failed to sign up');
+                        setTimeout(() => {
+                            setEmailError('');
+                        }, 5000);
+
+                    } else if (responseData.error === 'Username is taken') {
+                        setUsernameError(responseData.error);
+
+                        setTimeout(() => {
+                            setUsernameError('');
+                        }, 5000);
+
+                    } else {
+                        setError(responseData.error);
+
+                        setTimeout(() => {
+                            setError('');
+                        }, 5000);
+                    }
+                } else {
+                    setError('Failed to sign up');
+
+                    setTimeout(() => {
+                        setError('');
+                    }, 5000);
+                }
             }
-        }
         } catch (error) {
             console.log('Error signing up:', error);
             // Handle network errors or other exceptions
@@ -122,28 +179,33 @@ const validateConfirmPassword = () => {
                         <svg class="icon left-20 max-sm:left-5" xmlns="http://www.w3.org/2000/svg" width="30" height="32" viewBox="0 0 24 24" fill="#0000"><path d="M12 12a5 5 0 1 0 0-10 5 5 0 0 0 0 10ZM20.59 22c0-3.87-3.85-7-8.59-7s-8.59 3.13-8.59 7" stroke="red" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path></svg>
                         <input className="input w-4/5 max-sm:w-full" type="text" placeholder="*Username" name="username"  value={formData.username} onChange={handleChange} />
                     </div>
+                    {emptyUsername && <p className="text-red-500 mb-3 absolute" style={{zIndex: "999"}}>Username is required</p>}
+                    {usernameError && <p className="text-red-500 mb-3 absolute" style={{zIndex: "999"}}>{usernameError}</p>}
 
                     <div className="group mt-5 flex justify-center">
                     <svg class="icon left-20 max-sm:left-5" xmlns="http://www.w3.org/2000/svg" width="30" height="32" viewBox="0 0 24 24" fill="none"><path d="M8.5 19H8c-4 0-6-1-6-6V8c0-4 2-6 6-6h8c4 0 6 2 6 6v5c0 4-2 6-6 6h-.5c-.31 0-.61.15-.8.4l-1.5 2c-.66.88-1.74.88-2.4 0l-1.5-2c-.16-.22-.53-.4-.8-.4Z" stroke="red" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"></path><path d="M15.996 11h.01M11.995 11h.01M7.995 11h.008" stroke="#FF8A65" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path></svg>
                         <input className="input w-4/5 max-sm:w-full" name="email" type="email" placeholder="*Email" value={formData.email} onChange={handleChange}/>
                     </div>
-
-                    {error && <p className="text-red-500 mb-3 absolute" style={{zIndex: "999"}}>{error}</p>}
+                    {emptyEmail && <p className="text-red-500 mb-3 absolute" style={{zIndex: "999"}}>Email is required</p>}
+                    {emailError && <p className="text-red-500 mb-3 absolute" style={{zIndex: "999"}}>{emailError}</p>}
 
                     <div className="group mt-5 flex justify-center">
                     <svg class="icon left-20 max-sm:left-5" xmlns="http://www.w3.org/2000/svg" width="30" height="32" viewBox="0 0 24 24" fill="none"><path d="M12 12a5 5 0 1 0 0-10 5 5 0 0 0 0 10Z" stroke="#FF8A65" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path><path d="m19.21 15.74-3.54 3.54c-.14.14-.27.4-.3.59l-.19 1.35c-.07.49.27.83.76.76l1.35-.19c.19-.03.46-.16.59-.3l3.54-3.54c.61-.61.9-1.32 0-2.22-.89-.89-1.6-.6-2.21.01Z" stroke="#FF8A65" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"></path><path d="M18.7 16.25c.3 1.08 1.14 1.92 2.22 2.22" stroke="#FF8A65" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"></path><path d="M3.41 22c0-3.87 3.85-7 8.59-7 1.04 0 2.04.15 2.97.43" stroke="red" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path></svg>
                         <input className="input w-4/5 max-sm:w-full" name="firstName" type="text" placeholder="*First Name" value={formData.firstName} onChange={handleChange} />
                     </div>
+                    {emptyFirstName && <p className="text-red-500 mb-3 absolute" style={{zIndex: "999"}}>First Name is required</p>}
 
                     <div className="group mt-5 flex justify-center">
                     <svg class="icon left-20 max-sm:left-5" xmlns="http://www.w3.org/2000/svg" width="30" height="32" viewBox="0 0 24 24" fill="none"><path d="M12 12a5 5 0 1 0 0-10 5 5 0 0 0 0 10Z" stroke="#FF8A65" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path><path d="m19.21 15.74-3.54 3.54c-.14.14-.27.4-.3.59l-.19 1.35c-.07.49.27.83.76.76l1.35-.19c.19-.03.46-.16.59-.3l3.54-3.54c.61-.61.9-1.32 0-2.22-.89-.89-1.6-.6-2.21.01Z" stroke="#FF8A65" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"></path><path d="M18.7 16.25c.3 1.08 1.14 1.92 2.22 2.22" stroke="#FF8A65" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"></path><path d="M3.41 22c0-3.87 3.85-7 8.59-7 1.04 0 2.04.15 2.97.43" stroke="red" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path></svg>
                         <input className="input w-4/5 max-sm:w-full" type="text" name="lastName" placeholder="*Last Name" value={formData.lastName} onChange={handleChange} />
                     </div>
+                    {emptyLastName && <p className="text-red-500 mb-3 absolute" style={{zIndex: "999"}}>Last Name is required</p>}
 
                     <div className="group mt-5 flex justify-center">
                     <svg class="icon left-20 max-sm:left-5" xmlns="http://www.w3.org/2000/svg" width="30" height="32" viewBox="0 0 24 24" fill="none"><path d="M10.49 2.23 5.5 4.11c-1.15.43-2.09 1.79-2.09 3.01v7.43c0 1.18.78 2.73 1.73 3.44l4.3 3.21c1.41 1.06 3.73 1.06 5.14 0l4.3-3.21c.95-.71 1.73-2.26 1.73-3.44V7.12c0-1.23-.94-2.59-2.09-3.02l-4.99-1.87c-.85-.31-2.21-.31-3.04 0Z" stroke="#FF8A65"stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path><path d="M12 12.5a2 2 0 1 0 0-4 2 2 0 0 0 0 4ZM12 12.5v3" stroke="red" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"></path></svg>
                         <input className="input w-4/5 max-sm:w-full" type="password" name="password" placeholder="*Password" value={formData.password} onChange={handleChange}/>
                     </div>
+                    {emptyPassword && <p className="text-red-500 mb-3 absolute" style={{zIndex: "999"}}>Password is required</p>}
 
                     <div className="group mt-5 flex justify-center">
                     <svg class="icon left-20 max-sm:left-5" xmlns="http://www.w3.org/2000/svg" width="30" height="32" viewBox="0 0 24 24" fill="none"><path d="M10.49 2.23 5.5 4.11c-1.15.43-2.09 1.79-2.09 3.01v7.43c0 1.18.78 2.73 1.73 3.44l4.3 3.21c1.41 1.06 3.73 1.06 5.14 0l4.3-3.21c.95-.71 1.73-2.26 1.73-3.44V7.12c0-1.23-.94-2.59-2.09-3.02l-4.99-1.87c-.85-.31-2.21-.31-3.04 0Z" stroke="#FF8A65"stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path><path d="M12 12.5a2 2 0 1 0 0-4 2 2 0 0 0 0 4ZM12 12.5v3" stroke="red" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"></path></svg>
@@ -151,9 +213,10 @@ const validateConfirmPassword = () => {
                         {confirmPasswordError && <p className="text-red-500 mt-20 ml-2 absolute" style={{zIndex: "999"}}>{confirmPasswordError}</p>}
 
                     </div>
+                    {emptyConfirmPassword && <p className="text-red-500 mb-3 absolute" style={{zIndex: "999"}}>Confirm Password is required</p>}
 
                     <div className="group mt-5 flex justify-center">
-                     {/* <CountrySelect value={formData.country} className="input w-4/5 max-sm:w-full" name="country" onChange={handleCountryChange} /> */}
+
 
                      <select id="country" name="country" className="bg-gray-100 mt-5 h-10 input  max-sm:w-full" value={formData.country} onChange={handleChange}>
                         {CountrySelect.map((country, index) => (
@@ -163,14 +226,18 @@ const validateConfirmPassword = () => {
                         ))}
                     </select>
                     </div>
+                    {emptyCountry && <p className="text-red-500 mb-3 absolute" style={{zIndex: "999"}}>Username is required</p>}
 
 
 
 
-                    {/* <div class="mt-5 float-right mr-14 max-sm:mr-2">
+                    <div class="mt-5 float-right mr-14 max-sm:mr-2">
                         <input type="checkbox" name="rememberme"/>
                         <label for="rememberme" class="text-red-500 font-semibold">Rremember Me</label>
-                    </div> */}
+                    </div>
+
+                    {error && <p className="text-red-500 mb-3 absolute" style={{zIndex: "999"}}>{error}</p>}
+
 
                     <div className="mt-20">
                         <button type="submit" className="bg-red-400 text-white w-72 h-10 rounded text-lg" >Register</button>
@@ -178,7 +245,7 @@ const validateConfirmPassword = () => {
 
                     <div className="mt-10 mb-5">
                     <p>Have an account?
-                     <Link className="underline max-sm:ml-0 ml-2 text-red-500">Login</Link>
+                     <Link to="/"  className="underline max-sm:ml-0 ml-2 text-red-500">Login</Link>
                        </p>
                     </div>
 
@@ -193,4 +260,4 @@ const validateConfirmPassword = () => {
     )
 }
 
-export default Login
+export default Signup
