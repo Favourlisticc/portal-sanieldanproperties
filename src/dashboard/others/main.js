ed81a720fe445cb4ec311adb65f857368453d43b
Lomoee <favoursunday600@gmail.com>
2024-03-11 00:55:52 +0100

    dashboard mobilr responsive

--- src/dashboard/others/main.js
+++ src/dashboard/others/main.js
@@ -4,17 +4,17 @@ import { Link } from "react-router-dom"
 const Dashboard = () => {
     return(
         <div className="mt-10 ">
-            <div className="flex justify-between mb-10">
+            <div className="flex justify-between mb-10 sm-max:flex-col sm-max:pl-4">
 
-            <div className=" bg-white rounded-md p-3 text-black w-1/2 h-24 mr-3">
+            <div className=" bg-white rounded-md p-3 text-black w-1/2 sm-max:w-80 h-24 mr-3 sm-max:mr-0">
                    <p className=" text-left text-gray-600 text-xl ">0.00NGN</p>
                    <p className=" text-left text-xl mt-5 font-semibold">Current Account Balance</p>
 
                 </div>
 
-            <div className=" bg-white rounded-md p-3 text-black w-1/2 h-24">
+            <div className=" bg-white rounded-md p-3 text-black w-1/2 h-24 sm-max:w-80 sm-max:mt-3">
                    <p className=" text-left text-gray-600 text-xl">0.00NGN</p>
-                   <p className=" text-left text-lg mt-5 font-semibold">Withdrawn Earnings by Now (total Transactions)</p>
+                   <p className=" text-left text-lg mt-5 font-semibold sm-max:text-xs">Withdrawn Earnings by Now (total Transactions)</p>
 
                 </div>
 
@@ -22,42 +22,42 @@ const Dashboard = () => {
 
             </div>
 
-            <div className="flex flex-wrap justify-between">
+            <div className="flex flex-wrap justify-between sm-max:flex-col sm-max:flex-nowrap sm-max:pl-4">
 
-                <div className=" bg-green-500 rounded-md p-3 text-gray-50">
+                <div className=" bg-green-500 rounded-md p-3 text-gray-50 sm-max:w-80 ">
                    <p className=" text-left">0</p>
-                   <p className=" text-left">Total Referrals</p>
-                   <p>rewards and commissions recieved by now</p>
+                   <p className=" text-left ">Total Referrals</p>
+                   <p className="text-left sm-max:text-xs">rewards and commissions recieved by now</p>
                 </div>
 
-                <div className=" bg-orange-400 rounded-md p-3 text-gray-50">
+                <div className=" bg-orange-400 rounded-md p-3 text-gray-50 sm-max:w-80 sm-max:mt-5">
                    <p className=" text-left">0</p>
                    <p className=" text-left">Paid Referrals</p>
-                   <p>withdrawn number of referrals until now</p>
+                   <p className="text-left sm-max:text-xs">withdrawn number of referrals until now</p>
                 </div>
 
-                <div className="bg-red-500 rounded-md p-3 text-gray-50">
+                <div className="bg-red-500 rounded-md p-3 text-gray-50 sm-max:w-80 sm-max:mt-5">
                    <p className=" text-left">0</p>
                    <p className=" text-left">UnPaid Referrals</p>
-                   <p>which have been not withdrawn yet</p>
+                   <p className="text-left sm-max:text-xs">which have been not withdrawn yet</p>
                 </div>
 
-                <div className=" bg-blue-500 rounded-md p-3 text-gray-50 mt-3 w-80 h-20">
+                <div className=" bg-blue-500 rounded-md p-3 text-gray-50 mt-3 w-80 h-20 sm-max:w-80 sm-max:mt-5">
                    <p className=" text-left">0</p>
-                   <p className=" text-left">Total Payout Transactions</p>
+                   <p className="text-left sm-max:text-xs mt-3">Total Payout Transactions</p>
 
                 </div>
 
 
             </div>
 
-            <p className="text-left mt-5 dark:text-white/30">You can learn more about Affiliate program and to start earning referrals <Link href="/Learn more" className="text-red-500 underline">here</Link> </p>
+            <p className="text-left mt-5 dark:text-white/30 text-xs text-wrap sm-max:w-80">You can learn more about Affiliate program and to start earning referrals <Link href="/Learn more" className="text-red-500 underline">here</Link> </p>
 
-            <div className="flex mt-5">
+            <div className="flex mt-5 sm-max:flex-col sm-max:pl-4">
 
-                <div className="bg-white p-2 w-2/3  rounded-md">
-                    <p className="text-left flex text-blue-700 font-bold">Earn Overview <p className=" text-gray-500 font-normal"> (for Last 30 days)</p></p>
-                    <p className="text-left">Line Graph for Earnings back to 30 days.</p>
+                <div className="bg-white p-2 w-2/3 sm-max:w-80 rounded-md">
+                    <p className="text-left flex text-blue-700 font-bold sm-max:text-xs">Earn Overview <p className=" text-gray-500 font-normal"> (for Last 30 days)</p></p>
+                    <p className="text-left sm-max:text-xs">Line Graph for Earnings back to 30 days.</p>
 
                     <hr className="mt-72 mb-1"/>
 
@@ -76,39 +76,41 @@ const Dashboard = () => {
                     </div>
                 </div>
 
-                <div className="bg-white rounded-md ml-10 w-96">
+                <div className="bg-white rounded-md ml-10 sm-max:ml-0 sm-max:w-80  w-96 h-full pb-20 sm-max:mt-6">
                 <h2 className="pt-3 pl-5 font-bold text-blue-800 text-left mb-3">Summary for This Month</h2>
 
                 <hr/>
 
-                <div className="">
-                    <div className="flex  mt-1.5 mb-1.5">
+                <div className="ml-3.5 mr-3.5">
+                    <div className="flex  mt-1.5 mb-1.5 ml-5 justify-between mr-10">
                         <p>Total Referrals: </p>
-                        <p>0</p>
+
+                        <p className="">0</p>
+
                     </div>
                     <hr/>
 
-                    <div className="flex  mt-1.5 mb-1.5">
+                    <div className="flex  mt-1.5 mb-1.5 ml-5 justify-between mr-10">
                         <p>Total Earnings: </p>
-                        <p>0.00NGN</p>
+                        <p className="">0.00NGN</p>
                     </div>
                     <hr/>
 
-                    <div className="flex  mt-1.5 mb-1.5">
+                    <div className="flex  mt-1.5 mb-1.5 ml-5 justify-between mr-10">
                         <p>UnVerified Referrals: </p>
-                        <p>0</p>
+                        <p className="">0</p>
                     </div>
                     <hr/>
 
-                    <div className="flex mt-1.5 mb-1.5">
+                    <div className="flex mt-1.5 mb-1.5 ml-5 justify-between mr-10">
                         <p>Clicks: </p>
-                        <p>0</p>
+                        <p className="">0</p>
                     </div>
                     <hr/>
 
-                    <div className="flex mt-1.5 mb-1.5">
+                    <div className="flex mt-1.5 mb-1.5 ml-5 justify-between mr-10">
                         <p>Conversion: </p>
-                        <p>0%</p>
+                        <p className="">0%</p>
                     </div>
                     <hr/>
                 </div>
--- src/footer/footer.js
+++ src/footer/footer.js
@@ -11,7 +11,7 @@ const Footer = () => {
             <li>
               <a
                 target="blank"
-                href="mailto:hello@simmmple.com"
+                href="/"
                 className="text-base font-medium text-gray-600 hover:text-gray-600"
               >
                 Support
@@ -20,7 +20,7 @@ const Footer = () => {
             <li>
               <a
                 target="blank"
-                href="https://simmmple.com/licenses"
+                href="/"
                 className="text-base font-medium text-gray-600 hover:text-gray-600"
               >
                 License
@@ -29,21 +29,13 @@ const Footer = () => {
             <li>
               <a
                 target="blank"
-                href="https://simmmple.com/terms-of-service"
+                href="/"
                 className="text-base font-medium text-gray-600 hover:text-gray-600"
               >
                 Terms of Use
               </a>
             </li>
-            <li>
-              <a
-                target="blank"
-                href="https://blog.horizon-ui.com/"
-                className="text-base font-medium text-gray-600 hover:text-gray-600"
-              >
-                Blog
-              </a>
-            </li>
+
           </ul>
         </div>
       </div>
